7e2ae079473341c7840b62019fb6ef2664f8067f
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 01:06:43 +0530

    Initialise frontend

---
+++ cadence-fun/web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './component/Main'
+import Footer from './component/Footer'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+    <Main/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
