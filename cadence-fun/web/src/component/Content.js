565c0f7dc10ce70f3deea68754967dddfe96ee0e
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 01:05:19 +0530

    Add Content Component

---
+++ cadence-fun/web/src/component/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './Card'
+import "./Content.css"
+
+const Content = () => {
+  return (
+    <div className='content'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default Content
