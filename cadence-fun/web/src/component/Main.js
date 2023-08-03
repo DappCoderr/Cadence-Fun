b8eeceaa2872c92dab6019fb1a5cb71ab57e7a7c
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 01:04:48 +0530

    Add Main folder

---
+++ cadence-fun/web/src/component/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Content from './Content'
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <Header/>
+        <Content/>
+    </div>
+  )
+}
+
+export default Main
