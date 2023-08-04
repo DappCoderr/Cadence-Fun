e163f41513b66ee0faeb88e6bd5425b87f4173d5
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 20:46:17 +0530

    Update index.js

--- cadence-fun/web/src/index.js
+++ cadence-fun/web/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {AppRouter} from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={AppRouter}/>
   </React.StrictMode>
 );
 
