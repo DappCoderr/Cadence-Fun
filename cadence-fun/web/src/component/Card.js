5c8921d4b2b7c5c944c6a3651d3517d31a951ee6
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 20:43:02 +0530

    Update lesson card

--- cadence-fun/web/src/component/Card.js
+++ cadence-fun/web/src/component/Card.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import "./Card.css"
 
-const Card = () => {
+const Card = ({course, subText}) => {
   return (
     <div className='card'>
         <div className='card-container'>
-            <div>
-                <h1>Card 1</h1>
-            </div>
-            <div>
-                <p>Card sub text</p>
+            {/* <div className='card-image'>
+              <img src="https://cdn.dribbble.com/users/1605843/screenshots/3351502/hello-01.png?compress=1&resize=400x300"/>
+            </div> */}
+            <div className='card-title'>
+                <h1>{course}</h1>
+                <p>{subText}</p>
+                <button className='card-btn'>Start</button>
             </div>
         </div>
     </div>
