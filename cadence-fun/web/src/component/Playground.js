d21ea0d59e0326af4ed6e52a77910980f6c98a34
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 20:46:03 +0530

    Add playground page

---
+++ cadence-fun/web/src/component/Playground.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Playground.css"
+
+const Playground = () => {
+  return (
+    <div className='playground'>
+        <div>
+            <a className='play-link' href='https://play.flow.com/'>Playgound</a>
+        </div>
+        <div>
+            <a className='solution-link' href='https://play.flow.com/'>Solution</a>
+        </div>
+    </div>
+  )
+}
+
+export default Playground
