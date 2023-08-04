0e7d3aaa96116f9eefc61e46f741c85c814d5b06
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-08-04 20:44:43 +0530

    Update content with cards

--- cadence-fun/web/src/component/Content.js
+++ cadence-fun/web/src/component/Content.js
@@ -5,14 +5,15 @@ import "./Content.css"
 const Content = () => {
   return (
     <div className='content'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <div className='content-title'>
+        <h1>Learn Cadence by Building a Game!!</h1>
+      </div>
+      <div className='content-card'>
+        <Card course="Lesson 1" subText={"Basic about cadence smart contract language"}/>
+        <Card course="Lesson 2" subText={"(Coming Soon)"}/>
+        <Card course="Lesson 3" subText={"(Coming Soon)"}/>
+        <Card course="Lesson 4" subText={"(Coming Soon)"}/>
+      </div>
     </div>
   )
 }
